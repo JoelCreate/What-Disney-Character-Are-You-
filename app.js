f16be09939df662e10857efaa3854395beca449a
Joel Wiesner <j.wiesner88@gmail.com>
2023-07-01 13:40:56 -0700

    got character data and displayed on web page

---
+++ app.js
@@ -0,0 +1,23 @@
+const characters = document.getElementById("char-list")
+
+fetch("https://api.disneyapi.dev/character")
+    .then(response => response.json())
+    .then(charData => {
+            const charArr = charData.data
+            console.log(charArr)
+
+            let html = ""
+
+            for (let charInfo of charArr) {
+                html += `
+                    <h1>${charInfo.name}</h1>
+                    <img class="char-img" src="${charInfo.imageUrl}">
+                `
+            }
+
+            characters.innerHTML = html
+
+       
+        
+    })
+    
